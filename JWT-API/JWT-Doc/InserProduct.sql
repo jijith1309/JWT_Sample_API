8a35e70a3824f6bad3c6a9de4de9f57b881a868a
jijith <jijith.shanmughan@interfacesys.com>
2020-06-04 11:52:49 +0530

    sql

--- JWT-API/JWT-Doc/InserProduct.sql
+++ JWT-API/JWT-Doc/InserProduct.sql
@@ -2,6 +2,11 @@ select * from applicationusers
 -- delete from applicationusers
 
 select * from PRODUCTS
+select * from shoppingcarts
+select * from cartitems
+
+select * from orders
+select * from orderdetails
 
 INSERT INTO PRODUCTS(category,Name,Price,Description,Quantity,LastUpdatedOn,ProductImagePath)
-VALUES('Vegetarian','Veg Biriyani',100,'Spicy Veg Biriyani',5,NOW(),'https://www.mygingergarlickitchen.com/wp-content/uploads/2018/04/veg-briyani-recipe-restaurant-style-hyderabadi-veg-dum-biryani-recipe-4.jpg');
+VALUES('NonVegetarian','Shawarma',200,'Spicy Shawarma',5,NOW(),'https://www.thespruceeats.com/thmb/qWxT_QU0PlqphVPfgMtfQjSKPO0=/1867x1867/smart/filters:no_upscale()/chicken-shawarma-138740297-58260f943df78c6f6acabb5b.jpg');
